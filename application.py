88e78a53c615c9596d7a1e1a8197d618e4147930
dhruv <dhruvsaini2209@gmail.com>
2024-08-04 22:53:48 +0530

    adding more fratures

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ from flask import Flask,request,jsonify,render_template
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.linear_model import Ridge
 
 application = Flask(__name__)
 app=application
@@ -32,6 +32,7 @@ def predict_datapoint():
         ISI =float(request.form.get('ISI'))
         Classes=float(request.form.get('Classes'))
         Region=float(request.form.get('Region'))
+
 
         new_data_scaled = standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region ]])
         result=ridge_model.predict(new_data_scaled)
